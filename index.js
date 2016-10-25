97596395d50e98e904f66233e06cdd2a194cec3b
testing005 <xzinehacker@gmail.com>
2016-10-25 17:18:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ app.get("/price",function(req,res){
 	var name=req.query.name;
 	market.getItemPrice(570,name,function(err,data){
 		if(!err){
-			res.send(data);
 			res.send("Hello this works but not above");
+			res.send(data);
+			
 			res.close();
 		}
 	});
