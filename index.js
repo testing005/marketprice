988bd6aa5f23fd4f97614e9fc2d5cdee085216fd
unknown <xzinehacker@gmail.com>
2016-10-25 14:28:08 +0530

    adding new files

---
+++ index.js
@@ -0,0 +1,19 @@
+var http=require('http');
+var port=9000;
+var express=require('express');
+var app=new express();
+var market=require('steam-market-pricing');
+
+app.listen(process.env.port||5000,function(){
+	console.log("running on port 5000");
+});
+
+app.get("/",function(req,res){
+	var name=req.param('name');
+	market.getItemPrice(570,name,function(err,data){
+		if(!err){
+			res.json(data);
+		}
+	});
+
+});
