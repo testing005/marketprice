f743b3258bb16dc32ba244264d3b823ff4de3c70
testing005 <xzinehacker@gmail.com>
2016-10-27 00:21:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ app.get("/price",function(req,res){
 		for(var i in item) {
         //res.write(" "+"\""+item[i]+"\"" + ':' +"\""+data[item[i]]['median_price']+"\"");
 			pdata[item[i]]=data[item[i]]['median_price'];
-			res.write(""+pdata[item[i]]);
-    }
+		}
 		 res.write(""+JSON.stringify(pdata));
 		//res.write("}}");
 			
