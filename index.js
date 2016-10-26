6b84389882a05c482c8af592f3874f6e54ec8363
testing005 <xzinehacker@gmail.com>
2016-10-26 11:15:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.listen(process.env.PORT||5000,function(){
 app.get("/price",function(req,res){
 	var name=req.query.name;
 	var items=name.split(",");
-	console.log(name);
-	market.getItemPrice(570,items,function(err,data){
+	console.log(items);
+	/*market.getItemPrice(570,items,function(err,data){
 		if(!err){
 			res.writeHead(200,{"Content-Type":"application/json"});
 			res.write("{\"price\""+":\""+data.median_price+"\"}");
@@ -21,6 +21,6 @@ app.get("/price",function(req,res){
 		res.write(err);
 		}
 	});
-
+*/
        
 });
