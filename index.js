37a8a112e6f689bc4e274e2dff71c4f0665e9e15
testing005 <xzinehacker@gmail.com>
2016-10-26 14:11:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,20 +10,12 @@ app.listen(process.env.PORT||5000,function(){
 
 app.get("/price",function(req,res){
 	var name=req.query.name;
-	//console.log(name);
 	var item=name.split(",");
-	//var items[];
-         //item.forEach(name){items.push(name);}
-	//res.write(Buffer.from(items));
-	//console.log(items);
-	var test={"name":"nodejstesting","host":"heroku"};
-	res.write(JSON.stringify(test));
-	//res.write(items);
 	market.getItemsPrice(570,item,function(data){
-		/*console.log(data);
+		console.log(data);
 			 for(var i in item) {
         console.log(item[i] + ' median price: ' + data[item[i]]['median_price']);
-    }*/
+    }
                         res.writeHead(200,{"Content-Type":"application/json"});
 			res.write(data);
 			res.end();
